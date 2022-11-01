1da6e71a96d306d83d50608df3dc15c4d30fcbf4
unknown <toghrul@code.edu.az>
2022-11-01 07:36:19 +0400

    .

--- Js/index.js
+++ Js/index.js
@@ -4,26 +4,23 @@ const bigImg = document.querySelector(".slider img");
 imageOnPhotos.forEach((img)=>{
     img.addEventListener("click",function(e){
         e.preventDefault();    
-        // oz funksionalligin elinden alag
 console.log(this);
-// console.log(this.href);
 let imgSrc = this.getAttribute("href");
 console.log(bigImg);
 bigImg.setAttribute("src" ,imgSrc);
 popup.style.display ="flex";
-// console.log(imgSrc);
-
-
-
 
     });
 });
-
 document.addEventListener("click",(e)=>{
-    
     if(e.target.classList.contains("popup")){
         console.log(e.target);
         popup.style.display="none";
-
     }
 });
+
+// document.addEventListener("click",function(){
+//     popup.style.display="none";
+// })
+
+
